632a204f6dd78aa32afdfee0c7dd30ed903cb7c1
Hari Hari <clerryha@gmail.com>
2018-03-22 16:48:10 +0300

    modify models.py

--- app/api/models.py
+++ app/api/models.py
@@ -8,9 +8,9 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(60))
-    email = db.Column(db.String(60), unique=True)
-    password = db.Column(db.String(60))
+    username = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(60), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
     businesses = db.relationship('Business', backref='user',
                                  lazy='dynamic')
 
