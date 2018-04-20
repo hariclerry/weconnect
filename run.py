3be17df1ce7a07358f9795f1064ef8f189b5fff1
Harriet <clerryha@gmail.com>
2018-04-20 12:30:47 +0300

    Modify run.py

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 """ Import the create_app method, create the app and start the server"""
 import os
-
+from flask import redirect
 from app import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
-
+@app.route('/')
+def index():
+    return redirect('')
 if __name__ == '__main__':
     app.run()
