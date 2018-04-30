74deef5fa62e79b84dc1e89bf2837358d60b1395
Harriet <clerryha@gmail.com>
2018-04-30 13:37:24 +0300

    Modify business_service.py

--- app/api/service/business_service.py
+++ app/api/service/business_service.py
@@ -9,17 +9,17 @@ class BusinessService(object):
         self.arg = arg
 
 
-    # paginante businesses
+    # Paginante businesses
     @staticmethod
     def get_businesses(page, limit, search_string, location, category):
         """docstring for paginating through the business"""
         filters = {}
-        # generate filters
+        # Generate filters
         if location is not None:
-            filters["location"] = location
+            filters['location'] = location
         if category is not None:
-            filters["category"] = category
+            filters['category'] = category
         res = Business.get_businesses(page, limit, search_string, filters)
-        if res["status"]:
+        if res['status']:
             return jsonify(res), 200
         return jsonify(res), 404
